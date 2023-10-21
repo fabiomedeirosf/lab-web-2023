a40a5a7bca6cdbab82cbde6730d71274d5964f20
Fabio Faria <fabio.faria@luizalabs.com>
2023-10-21 11:57:34 -0300

    feat: add jwt

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const Hapi = require("@hapi/hapi");
 const routes = require('./config/routes');
 const config = require('./config/env-config')
+const HapiJWT = require('hapi-auth-jwt2');
+
 const server = Hapi.server({
     port: config.port,
     host: config.host
@@ -14,4 +16,22 @@ const plugins = [
         }
     }
 ];
+
+const validate = async (decoded, request, h) => {
+    console.log('validate');
+    console.log(decoded);
+
+    return {isValid: true};
+}
+
+server.register(HapiJWT);
+
+server.auth.strategy('jwt', 'jwt',
+{
+    key: 'empresario',
+    validate
+});
+
+server.auth.default('jwt');
+
 module.exports = {server, plugins};
